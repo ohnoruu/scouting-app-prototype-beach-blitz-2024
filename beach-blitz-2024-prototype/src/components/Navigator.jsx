e925ab6f69e99390d415694f5456fb782ac28389
ohnoruu <rubydao007@gmail.com>
2024-10-31 16:15:19 -0700

    updated navigator

--- beach-blitz-2024-prototype/src/components/Navigator.jsx
+++ beach-blitz-2024-prototype/src/components/Navigator.jsx
@@ -1,41 +1,40 @@
-//Navigation Bar
 import React from 'react';
-import { BrowserRouter as NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import IonIcon from '@reacticons/ionicons';
 import './Navigator.css';
 
 export default function Navigator() {
   return (
-      <div className="tabNavigator">
-        <nav className="tabBar">
-          <NavLink 
+    <div className="tabNavigator">
+      <nav className="tabBar">
+        <NavLink 
           to="/navigator/home" 
           className={({ isActive }) => isActive ? 'activeTab' : 'tabLink'}
-          >
-            <IonIcon name="home-outline" className="icon"/>
-          </NavLink>
-          <NavLink 
+        >
+          <IonIcon name="home-outline" className="icon"/>
+        </NavLink>
+        <NavLink 
           to="/navigator/search" 
-          className = {({ isActive }) => isActive ? 'activeTab' : 'tabLink'}
-          >
-            <IonIcon name="search-outline" className="icon"/>
-          </NavLink>
-          <NavLink 
+          className={({ isActive }) => isActive ? 'activeTab' : 'tabLink'}
+        >
+          <IonIcon name="search-outline" className="icon"/>
+        </NavLink>
+        <NavLink 
           to="/navigator/record" 
-          className= {({ isActive }) => isActive ? 'activeTab' : 'tabLink'}
-          >
-            <IonIcon name="add-circle-outline" className="icon"/>
-          </NavLink>
-          <NavLink 
+          className={({ isActive }) => isActive ? 'activeTab' : 'tabLink'}
+        >
+          <IonIcon name="add-circle-outline" className="icon"/>
+        </NavLink>
+        <NavLink 
           to="/navigator/settings" 
-          className= {({ isActive }) => isActive ? 'activeTab' : 'tabLink'}
-          >
-            <IonIcon name="settings-outline" className="icon"/>
-          </NavLink>
-        </nav>
-        <div className="tabContent">
-          <Outlet/>
-        </div>
+          className={({ isActive }) => isActive ? 'activeTab' : 'tabLink'}
+        >
+          <IonIcon name="settings-outline" className="icon"/>
+        </NavLink>
+      </nav>
+      <div className="tabContent">
+        <Outlet/>
       </div>
+    </div>
   );
 }
